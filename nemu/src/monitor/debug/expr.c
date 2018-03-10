72a1bf41e4ef35f9c4d8d77dfa4aa70d97853d84
tracer-ics2017 <tracer@njuics.org>
2018-03-10 07:14:57 +0000

    >  compile 161630231 liangjiajuan Linux a56c75facae1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  07:14:57 up  4:01,  4 users,  load average: 0.00, 0.00, 0.00 2127798bdae80750d4449fcfbca82407f8986297

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -186,6 +186,7 @@ uint32_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   else {
       int i;
+      printf("nr_token: %d\n",nr_token);
       for (i=0;i<nr_token;i++){
           printf("%s",tokens[i].str);
       }
