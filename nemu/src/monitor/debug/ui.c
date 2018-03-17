5caaea43c3bc02e84973451f4a55be3b267e6f17
tracer-ics2017 <tracer@njuics.org>
2018-03-17 02:44:08 +0000

    >  compile 161630231 liangjiajuan Linux a56c75facae1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  02:44:08 up  2:01,  4 users,  load average: 0.00, 0.00, 0.00 a5e3b5ee131e01c232ead31c3d4d52c2f26ffc09

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -189,7 +189,7 @@ static int cmd_p(char *args){
     }
     else{
         bool succeed;
-        printf("%08x   %d\n",expr(args,&succeed),expr(args,&succeed));
+        printf("0x%08x   %d\n",expr(args,&succeed),expr(args,&succeed));
         
     }
     return 0;
