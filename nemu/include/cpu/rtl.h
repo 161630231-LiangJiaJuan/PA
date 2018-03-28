2938a24d7b3ce14d3cb919e5e0a8174a5013b554
tracer-ics2017 <tracer@njuics.org>
2018-03-28 14:41:04 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  14:41:04 up 7 min,  5 users,  load average: 0.00, 0.30, 0.24 22dc5598f58ff5e93f7a4d84dbfc88158b008edd

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -140,11 +140,11 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
-  // esp <- esp - 4
-  // M[esp] <- src1
-    rtl_lr_l(&t0,R_ESP);
-    t0=t0-4;
-    rtl_sr_l(R_ESP,&t0);
+  cpu. esp =cpu.esp - 4;
+ rtl_sm(&cpu.esp,4,src1); 
+  //  rtl_lr_l(&t0,R_ESP);
+   // t0=t0-4;
+   // rtl_sr_l(R_ESP,&t0);
    // SP=*src1;
     
 }
