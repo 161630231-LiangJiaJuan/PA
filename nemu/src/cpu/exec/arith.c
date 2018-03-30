8f51cbc84d3f293f8acc10f46ebce39290de6ace
tracer-ics2017 <tracer@njuics.org>
2018-03-30 15:33:21 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  15:33:21 up  2:42,  5 users,  load average: 0.00, 0.00, 0.00 d4f2500a3e022eca2640d16b9aaae49d2f69b68a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,8 +7,8 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-// rtl_sub (&id_dest->val,&id_dest->val,&id_src->val);
- // print_asm_template2(sub);
+ rtl_sub (&id_dest->val,&id_dest->val,&id_src->val);
+  print_asm_template2(sub);
 }
 
 make_EHelper(cmp) {
