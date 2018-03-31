36704fe998db93fe7f58426aad0039ec75160904
tracer-ics2017 <tracer@njuics.org>
2018-03-31 15:37:03 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  15:37:03 up  1:15,  5 users,  load average: 0.00, 0.00, 0.00 bb099c3be7e977bdc54f4edd0cf17236e4e73c9

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,6 +7,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
+ printf("%x %x %x \n",id_dest->val,id_dest->val,id_src->val);
  rtl_sub (&id_dest->val,&id_dest->val,&id_src->val);
  rtl_sr_l(R_ESP,&id_dest->val);
  printf("%x %x %x \n",id_dest->val,id_dest->val,id_src->val);
