8cb6e1a0350b4dcc3c9d51d25ceb88b053ceda68
tracer-ics2017 <tracer@njuics.org>
2018-04-21 16:31:15 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  16:31:15 up  2:07,  5 users,  load average: 0.00, 0.00, 0.00 2bd9798ad07bd7ab4c8bcee52fbcbbe112130438

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -49,6 +49,7 @@ make_EHelper(ret) {
 make_EHelper(call_rm) {
  rtl_push(&decoding.seq_eip); 
 decoding.is_jmp=1;
+printf("%x  %x",id_dest->val,*eip);
 decoding.jmp_eip=id_dest->val+*eip;
   print_asm("call *%s", id_dest->str);
 }
