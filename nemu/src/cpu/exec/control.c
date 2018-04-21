b741c6218b261d21e168d49fff2b36472e03248e
tracer-ics2017 <tracer@njuics.org>
2018-04-21 05:25:55 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  05:25:55 up 5 min,  4 users,  load average: 0.04, 0.28, 0.18 e6aee55812c0dccdc66b557f8830b184a02e652c

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,7 +25,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
 
-rtl_push(eip);
+rtl_push(&decoding.seq_eip);
 decoding.is_jmp=1;
 decoding.jmp_eip=id_dest->val+*eip; //id_dest->val;
   print_asm("call %x", decoding.jmp_eip);
@@ -41,7 +41,6 @@ make_EHelper(ret) {
  else{
  rtl_pop(&t0);
  decoding.jmp_eip = t0;
-
  }
   print_asm("ret");
 }
