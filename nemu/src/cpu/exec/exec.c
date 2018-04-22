8299e92ea13b3b5c2b2f260144411e97b7067f61
tracer-ics2017 <tracer@njuics.org>
2018-04-22 15:02:02 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  15:02:02 up 20 min,  4 users,  load average: 0.00, 0.01, 0.05 24b23538a3f22693de65ca7ded4cc1cc1142576

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -61,7 +61,7 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    IDEX(I,inc), IDEX(E,dec),EX(call_rm) , IDEX(E,call),
+    EX(inc), IDEX(E,dec),EX(call_rm) , IDEX(E,call),
     IDEX(E,jmp), IDEX(E,jmp), EX(push), EMPTY)
 
   /* 0x0f 0x01*/
