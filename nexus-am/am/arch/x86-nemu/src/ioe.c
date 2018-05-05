1c2da2428b00cd790cf00fcc6632d6eccd2fae08
tracer-ics2017 <tracer@njuics.org>
2018-05-05 13:46:27 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  13:46:27 up  2:45,  4 users,  load average: 0.34, 0.45, 0.96 4495b7675c156f9ffc9b7b99e3ceeb1de1943e8

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -33,7 +33,7 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
     //uint32_t *p = fb;
    // pixels=fb;
 
-    int cp_bytes=sizeof(uint32_t) * min (w*4,_screen.width - x);
+    int cp_bytes=min (w*4,_screen.width - x);
     for(int i=0;i<h;i++){
        // if(me)
         //memcpy(&fb[x+(y+i)*_screen.width],pixels,w);
