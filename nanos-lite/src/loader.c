0a2c50ab85703025a46411114040bfe6a28a3b59
tracer-ics2017 <tracer@njuics.org>
2018-05-17 14:36:51 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  14:36:51 up 14 min,  4 users,  load average: 0.16, 0.29, 0.25 233e644eb5ad2170e3b108487f60dff2204860e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
-void ramdisk_read(void *buf,off_t offset,size_t len);
-size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
+extern size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
   ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size()); 
   return (uintptr_t)DEFAULT_ENTRY;
