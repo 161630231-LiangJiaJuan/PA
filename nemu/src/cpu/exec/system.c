01c6b889d81330580cd14df0635f1fcae8e0f86e
tracer-ics2017 <tracer@njuics.org>
2018-05-18 04:44:05 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  04:44:05 up  2:41,  6 users,  load average: 0.19, 0.05, 0.01 c5697fa958f59671d332c82133c6aec83e1cfaf8

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -4,7 +4,15 @@ void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
-  TODO();
+  t0=(vaddr_read(id_dest->addr,2));
+   if(id_dest->width== 2){
+     t1=(vaddr_read(id_dest->addr,3));
+   }
+   else
+  t1=(vaddr_read(id_dest->addr+2,4)); 
+
+    cpu.limit=t0;
+    cpu.base=t1;
 
   print_asm_template1(lidt);
 }
@@ -25,8 +33,9 @@ make_EHelper(mov_cr2r) {
 #endif
 }
 
+void raise_intr(uint8_t NO,vaddr_t ret_addr);
 make_EHelper(int) {
-  TODO();
+  raise_intr(id_dest->val,decoding.seq_eip); 
 
   print_asm("int %s", id_dest->str);
 
--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -1,4 +1,5 @@
 #include "cpu/exec.h"
+#include "cpu/rtl.h"
 #include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
@@ -6,8 +7,21 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+
+    uint32_t low=vaddr_read(cpu.base+NO*sizeof(GateDesc),4);
+    uint32_t high=vaddr_read(cpu.base+NO*sizeof(GateDesc)+4,4);
+    assert((high<<16) >>31 == 1);
+    rtl_push(&cpu.eflags);
+    uint32_t tmpCS=cpu.CS;
+    rtl_push(&tmpCS);
+    rtl_push(&ret_addr);
+    decoding.is_jmp=1;
+    decoding.jmp_eip =((high>>16)<<16)+((low<<16)>>16);
+    
+
 }
+
+
 
 void dev_raise_intr() {
 }
