f3e87b319edf895825fb44d4e08878cea1eedd34
tracer-ics2017 <tracer@njuics.org>
2018-05-19 05:13:37 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  05:13:37 up  2:07,  6 users,  load average: 0.34, 0.20, 0.08 1c1e7250e9f4aed4420c59ecba8684948d3e6381

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -9,9 +9,8 @@
 struct _RegSet {
 //  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
   uintptr_t eax,ecx,edx,ebx,esp,ebp,esi,edi;
-uintptr_t error_code;
 int irq;
- uintptr_t eflags,cs,eip;
+ uintptr_t error_code,eflags,cs,eip;
 };
 
 #define SYSCALL_ARG1(r) 0
