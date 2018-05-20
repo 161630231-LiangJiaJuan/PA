dd74409723ce0fdb955ddcfcff910a8ba41f6f02
tracer-ics2017 <tracer@njuics.org>
2018-05-20 03:58:53 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  03:58:53 up  2:52,  6 users,  load average: 0.00, 0.03, 0.08 f16ce59e2792661223d519620c926ec5ef559a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,11 @@
 #include "common.h"
 #include "syscall.h"
-
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
-    case SYS_none:printf("SYS_none \n"); break; 
+    case SYS_none:{printf("SYS_none \n");r->eax=1; break; }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
