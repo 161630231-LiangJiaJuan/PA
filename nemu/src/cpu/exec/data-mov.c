ca38b655542e1c9e3fa50bc96063f77f9f9de20a
tracer-ics2017 <tracer@njuics.org>
2018-05-20 01:53:33 +0000

    >  compile 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  01:53:33 up 47 min,  6 users,  load average: 0.14, 0.45, 0.28 a2d8e8e5807037247a05450d694b4c8a65f33e0

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -32,9 +32,9 @@ make_EHelper(pusha) {
 
 make_EHelper(popa) {
   for(int i=7;i>=0;i--){
-    if(i==R_ESP)
-       ; //continue;
-    else
+  //  if(i==R_ESP)
+   //    ; //continue;
+   // else
         rtl_pop(&reg_l(i));
   }
 
