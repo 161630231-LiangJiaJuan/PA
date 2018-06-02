df4e047145a01cbe19cfb706ac06add542280c52
tracer-ics2017 <tracer@njuics.org>
2018-06-02 02:15:38 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  02:15:38 up  1:46,  6 users,  load average: 0.00, 0.00, 0.00 6608b0071d7575095e5b3767629338f2ddc55d0b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,21 @@
 #include "common.h"
 #include "syscall.h"
 
-
+//uint32_t vaddr_read(vaddr_t addr ,int len);
 uintptr_t sys_none(){
     return 1;
+}
+
+ssize_t sys_write(int fd,const void *buf,size_t count){
+    if (fd==1 || fd == 2){
+        int i;
+        for (i=0;i<count;i++){
+            char c=*((char *)buf);
+            _putc(c);
+        }
+        return i;
+    }
+    return 0;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -16,6 +28,10 @@ _RegSet* do_syscall(_RegSet *r) {
 
     case SYS_exit:{
         _halt(0);
+        break;
+    }
+    case SYS_write:{
+        SYSCALL_ARG1(r)=sys_write(a[0],&a[1],a[2]);
         break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
