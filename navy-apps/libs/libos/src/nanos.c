c10b3bc6ec6802753b33f164dc0afaf8f8438d76
tracer-ics2017 <tracer@njuics.org>
2018-06-03 02:16:02 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  02:16:02 up  1:23,  5 users,  load average: 0.02, 0.31, 0.26 8459a1813539f87a2c839e9389180af074d2d37a

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -35,7 +35,7 @@ int _write(int fd, void *buf, size_t count){
 void *_sbrk(intptr_t increment){
   void *pre_end=_end;
   if(_syscall_( SYS_brk,increment,0,0)==0)
-      return pre_end;
+      return (void *)pre_end;
   else
   return (void *)-1;
 }
