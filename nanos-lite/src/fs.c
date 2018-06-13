19b10ed00ec3db536a3afd8736b2320bf776044d
tracer-ics2017 <tracer@njuics.org>
2018-06-13 11:25:58 +0000

    >  run 161630231 liangjiajuan Linux 4a3ff947dec1 4.4.115-boot2docker #1 SMP Thu Feb 8 17:36:45 UTC 2018 x86_64 GNU/Linux  11:25:58 up  2:03,  4 users,  load average: 0.01, 0.25, 0.62 2cfee55dc75b668e71b6eed26283b9dcca9175ba

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,9 +67,7 @@ ssize_t fs_read(int fd,void *buf,size_t len){
     else if(fd==FD_DISPINFO){
        // Log("FD_DISPINFO read");
         dispinfo_read(buf,file_table[fd].open_offset+file_table[fd].disk_offset,read_size);
-        Log("%s",buf);
         file_table[fd].open_offset+=read_size;
-        
         return read_size;
     }
     else{
